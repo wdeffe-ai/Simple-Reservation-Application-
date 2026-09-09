26166a9860526ae62932ffee0a207b341ac44916
Wade Deffenbaugh <wdeffe@gmail.com>
2026-09-08 21:01:43 -0400

    Create React application folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Reservation Application</h1>
+      <p>Welcome to the Private Club Reservation System</p>
+    </div>
+  );
+}
+
+export default App;
